b101137c9415e0e075bc2f4c91e1355c77e0079d
Bartłomiej Ściseł <01158955@pw.edu.pl>
2021-05-19 18:24:09 +0200

    Answer class implemented.

---
+++ include/answer.hpp
@@ -0,0 +1,28 @@
+#ifndef ANSWER_HPP
+#define ANSWER_HPP
+
+#include <string>
+
+class Answer
+{
+private:
+    std::string text;
+
+public:
+    Answer(const std::string &text) : text(text){};
+    std::string get_text() { return text; };
+};
+
+class Correct_answer : public Answer
+{
+public:
+    Correct_answer(const std::string &text) : Answer(text){};
+};
+
+class Wrong_answer : public Answer
+{
+public:
+    Wrong_answer(const std::string &text) : Answer(text){};
+};
+
+#endif
