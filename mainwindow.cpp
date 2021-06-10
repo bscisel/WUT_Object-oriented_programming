364a2ee3d269949087999ad57f31450d018f047b
Bartłomiej Ściseł <01158955@pw.edu.pl>
2021-06-10 17:03:08 +0200

    User and category selecting.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <QDialog>
+#include <QFileDialog>
 #include "mainwindow.h"
 #include "adduserdialog.h"
 #include "./ui_mainwindow.h"
 
 Q_DECLARE_METATYPE(std::shared_ptr<User>)
+Q_DECLARE_METATYPE(std::shared_ptr<Category>)
 
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
@@ -20,21 +22,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
     std::cout << database_users->get_users_number() << std::endl; 
 }
 
-void MainWindow::load_users_into_widgetlist() {
-    for (auto &user : database_users->get_users()) {
-        add_user_to_list(user, QString::fromStdString(user->get_name()));
-    }
-}
-
-void MainWindow::add_user_to_list(const std::shared_ptr<User> &user, const QString &name) {
-    QListWidgetItem *new_user_on_list = new QListWidgetItem(name);
-    QVariant data;
-    data.setValue(user);
-    new_user_on_list->setData(Qt::UserRole, data);
-    ui->usersListWidget->addItem(new_user_on_list);
-}
-
-
 MainWindow::~MainWindow()
 {
     delete ui;
@@ -44,14 +31,15 @@ MainWindow::~MainWindow()
     std::cout << "DESTRUCTOR" << std::endl;
 }
 
-void MainWindow::on_openFileButton_clicked()
-{
-    
-}
 
-void MainWindow::on_exitButton_clicked()
+////////////////////////////////////////////////////
+//                   1 PAGE                       //
+////////////////////////////////////////////////////
+
+void MainWindow::on_selectUserButton_clicked()
 {
-    close();
+    QListWidgetItem *user_item = ui->usersListWidget->currentItem();
+    set_user_as_active(user_item);
 }
 
 void MainWindow::on_addUserButton_clicked()
@@ -65,4 +53,116 @@ void MainWindow::on_addUserButton_clicked()
             add_user_to_list(pointer_to_user, entered_user_name);
         }
     }
+}
+
+void MainWindow::on_exitButton_clicked() {
+    close();
+}
+
+void MainWindow::on_newGameButton_clicked() {
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_usersListWidget_itemDoubleClicked(QListWidgetItem *user_item) {
+    if (user_item != active_user)
+        set_user_as_active(user_item);
+}
+
+void MainWindow::on_usersListWidget_itemSelectionChanged() {
+    ui->selectUserButton->setEnabled(true);
+    if (ui->usersListWidget->currentItem() == active_user)
+        ui->selectUserButton->setEnabled(false);
+}
+
+
+////////////////////////////////////////////////////
+//                   2 PAGE                       //
+////////////////////////////////////////////////////
+
+void MainWindow::on_selectCategoryButton_clicked() {
+    QListWidgetItem *item = ui->categoriesListWidget->currentItem();
+    set_category_as_selected(item);
+}
+
+void MainWindow::on_openFileButton_clicked()
+{
+    read_categories_from_file(QFileDialog::getOpenFileName(this, "Open database file", ".", "JSON Files (*.json)"));
+}
+
+void MainWindow::on_cancelButton_clicked() {
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_exitButton_2_clicked() {
+    close();
+}
+
+void MainWindow::on_categoriesListWidget_itemDoubleClicked(QListWidgetItem *item) {
+    if (item != selected_category)
+        set_category_as_selected(item);
+}
+
+void MainWindow::on_categoriesListWidget_itemSelectionChanged() {
+    ui->selectCategoryButton->setEnabled(true);
+    if (ui->categoriesListWidget->currentItem() == selected_category)
+        ui->selectCategoryButton->setEnabled(false);
+}
+
+////////////////////////////////////////////////////
+//                   FUNCTIONS                    //
+////////////////////////////////////////////////////
+
+std::shared_ptr<User> MainWindow::get_pointer_to_active_user() const {
+    return active_user->data(Qt::UserRole).value<std::shared_ptr<User>>();
+}
+
+std::shared_ptr<Category> MainWindow::get_pointer_to_selected_category() const {
+    return selected_category->data(Qt::UserRole).value<std::shared_ptr<Category>>();
+}
+
+void MainWindow::set_user_as_active(QListWidgetItem *user_item) {
+    active_user = user_item;
+    ui->activeUserLabel->setText(user_item->text());
+    ui->selectUserButton->setEnabled(false);
+    ui->newGameButton->setEnabled(true);
+}
+
+void MainWindow::set_category_as_selected(QListWidgetItem *item) {
+    selected_category = item;
+    ui->selectedCategoryLabel->setText(item->text());
+    ui->selectCategoryButton->setEnabled(false);
+    ui->startButton->setEnabled(true);
+}
+
+void MainWindow::load_users_into_widgetlist() {
+    for (auto &user : database_users->get_users()) {
+        add_user_to_list(user, QString::fromStdString(user->get_name()));
+    }
+}
+
+void MainWindow::read_categories_from_file(QString file_name) {
+    if (database->open_file(file_name.toStdString())) {
+        const auto &categories = database->get_categories();
+        auto i = categories.size();
+        database->read_data();
+        for ( ; i != categories.size(); i++) { // -_-
+            add_category_to_list(categories[i], QString::fromStdString(categories[i]->get_name()));
+        }
+    }
+}
+
+void MainWindow::add_user_to_list(const std::shared_ptr<User> &user, const QString &name) {
+    QListWidgetItem *new_user_on_list = new QListWidgetItem(name);
+    QVariant data;
+    data.setValue(user);
+    new_user_on_list->setData(Qt::UserRole, data);
+    ui->usersListWidget->addItem(new_user_on_list);
+}
+
+void MainWindow::add_category_to_list(const std::shared_ptr<Category> &category, const QString &name) {
+    QListWidgetItem *new_category_on_list = new QListWidgetItem(name);
+    QVariant data;
+    data.setValue(category);
+    new_category_on_list->setData(Qt::UserRole, data);
+    ui->categoriesListWidget->addItem(new_category_on_list);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidget>
 #include "database.hpp"
 #include "database_users.hpp"
 
@@ -21,16 +22,39 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_openFileButton_clicked();
-    void on_exitButton_clicked();
+    // 1 page
+    void on_selectUserButton_clicked();
     void on_addUserButton_clicked();
+    void on_exitButton_clicked();
+    void on_newGameButton_clicked();
+    void on_usersListWidget_itemDoubleClicked(QListWidgetItem *user_item);
+    void on_usersListWidget_itemSelectionChanged();
+    
+    // 2 page
+    void on_selectCategoryButton_clicked();
+    void on_openFileButton_clicked();
+    void on_cancelButton_clicked();
+    void on_exitButton_2_clicked();
+    void on_categoriesListWidget_itemDoubleClicked(QListWidgetItem *item);
+    void on_categoriesListWidget_itemSelectionChanged();
 
 private:
     Ui::MainWindow *ui;
     Database *database;
     Database_users *database_users;
+    QListWidgetItem *active_user;
+    QListWidgetItem *selected_category;
+    std::shared_ptr<User> get_pointer_to_active_user() const;
+    std::shared_ptr<Category> get_pointer_to_selected_category() const;
+    
+    void set_user_as_active(QListWidgetItem *user_item);
+    void set_category_as_selected(QListWidgetItem *item);
+    
     void load_users_into_widgetlist();
+    void read_categories_from_file(QString file_name);
+    
     void add_user_to_list(const std::shared_ptr<User> &user, const QString &name);
+    void add_category_to_list(const std::shared_ptr<Category> &category, const QString &name);
 };
 
 #endif
