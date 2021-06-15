9759766d774b584158ec072059ddf92c408f3737
Bartłomiej Ściseł <01158955@pw.edu.pl>
2021-06-15 11:58:44 +0200

    Database user test file added.

--- tests/source/database_users.cpp
+++ tests/source/database_users.cpp
@@ -13,7 +13,7 @@ TEST(database_user, create_users_database)
 TEST(database_users, read_from_file)
 {
     Database_users database_u;
-    database_u.open_file("users_test.json");
+    database_u.open_file("../../database_files/users_test.json");
     database_u.read_data();
     EXPECT_EQ(database_u.get_users_number(), 1);
     EXPECT_EQ(database_u.get_users()[0]->get_name(), "Natalia");
